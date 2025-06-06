8dbc85cb94e30969ca30c97488aaf6d21bf30dfb
Nitish <snkumar6926@gmail.com>
2025-06-06 15:11:29 +0530

    Updates added

--- public/main.js
+++ public/main.js
@@ -1,3 +1,36 @@
 function openModal() {
     alert("Project details will be available soon. Stay tuned!");
 }
+
+// ContactForm Listener
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('contactForm');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('cemail').value;
+        const phone = document.getElementById('uphone').value;
+        const message = document.getElementById('message').value;
+
+        // Make a POST request to your server for form submission
+        fetch('/contact', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name, email, phone, message }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.success) {
+                alert('Message Submitted Successfully!');
+                form.reset(); // Reset the form after successful submission
+            } else {
+                alert('Error: ' + data.message);
+            }
+        })
+        .catch((error) => console.error('Error:', error));
+    });
+});
