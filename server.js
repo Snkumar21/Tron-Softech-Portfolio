7954fdcf1941f9302162816a91173df320e976e6
Nitish <snkumar6926@gmail.com>
2025-06-13 15:26:59 +0530

    Updates added

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.get("/index", (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+app.listen(3001, () => {
+    console.log("Server is running on http://localhost:3001");
 });
